e676bb6dfb08849a012efc938dd1ba110c287181
Redhotsmasher <Red_hot_smasher@yahoo.com>
2015-06-02 19:35:52 +0200

    Added BGM. Also added a winning sound, though since you can't win yet I made it play on startup (feel free to kill that if it annoys you, my two lines of code are clearly labeled).

--- app/src/main/java/devgui2/devgui2_project/GridGame.java
+++ app/src/main/java/devgui2/devgui2_project/GridGame.java
@@ -2,13 +2,16 @@ package devgui2.devgui2_project;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.drawable.BitmapDrawable;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -26,33 +29,72 @@ public class GridGame extends Activity {
 
 	int canvasWidth;
 	int canvasHeight;
+	boolean sfx;
+	boolean bgm;
+	MediaPlayer musicPlayer;
+	MediaPlayer winPlayer;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gridgame);
-        final DrawView drawView = new DrawView(this, getIntent().getExtras());
-        drawView.setBackgroundColor(Color.BLACK);
-        setContentView(drawView);
-        float blockLength = drawView.getBlockLength();
-	    int gridWidth    = getIntent().getIntExtra("gridWidth",    5);
-	    int gridHeight   = getIntent().getIntExtra("gridHeight",   5);
-	    int blockMinSize = getIntent().getIntExtra("blockMinSize", 3);
-	    int blockMaxSize = getIntent().getIntExtra("blockMaxSize", 3);
-	    final Piece[] pieces = GridMaker.makeGrid(gridWidth, gridHeight, blockMinSize, blockMaxSize);
-	    drawView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-		    @Override
-		    public void onGlobalLayout() {
-			    drawView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
-			    canvasWidth  = drawView.getWidth();
-			    canvasHeight = drawView.getHeight();
-			    for (int i = 0; i < pieces.length; i++) {
-				    pieces[i].setX((int) (Math.random() * canvasWidth));
-				    pieces[i].setY((int) (Math.random() * canvasHeight));
-			    }
-			    drawView.setPieces(pieces);
-			    drawView.invalidate();
-		    }
-	    });
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+		sfx = sharedPrefs.getBoolean("pref_sound_fx", true);
+		bgm = sharedPrefs.getBoolean("pref_sound_music", true);
+		if (bgm == true) {
+			musicPlayer = MediaPlayer.create(this, R.raw.bgmloop);
+			musicPlayer.setLooping(true);
+			musicPlayer.start();
+		}
+		if (sfx == true) {
+			winPlayer = MediaPlayer.create(this, R.raw.sfxwin);
+		}
+		setContentView(R.layout.activity_gridgame);
+		final DrawView drawView = new DrawView(this, getIntent().getExtras());
+		drawView.setBackgroundColor(Color.BLACK);
+		setContentView(drawView);
+		float blockLength = drawView.getBlockLength();
+		int gridWidth = getIntent().getIntExtra("gridWidth", 5);
+		int gridHeight = getIntent().getIntExtra("gridHeight", 5);
+		int blockMinSize = getIntent().getIntExtra("blockMinSize", 3);
+		int blockMaxSize = getIntent().getIntExtra("blockMaxSize", 3);
+		final Piece[] pieces = GridMaker.makeGrid(gridWidth, gridHeight, blockMinSize, blockMaxSize);
+		drawView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+			@Override
+			public void onGlobalLayout() {
+				drawView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+				canvasWidth = drawView.getWidth();
+				canvasHeight = drawView.getHeight();
+				for (int i = 0; i < pieces.length; i++) {
+					pieces[i].setX((int) (Math.random() * canvasWidth));
+					pieces[i].setY((int) (Math.random() * canvasHeight));
+				}
+				drawView.setPieces(pieces);
+				drawView.invalidate();
+			}
+		});
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		if (bgm == true) {
+			musicPlayer.pause();
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if (bgm == true) {
+			musicPlayer.start();
+		}
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onStop();
+		if (bgm == true) {
+			musicPlayer.stop();
+		}
+	}
 }
