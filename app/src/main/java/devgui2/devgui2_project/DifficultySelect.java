93680f25f331b930c7a885317cc910a8e4091303
Andreas Wennerberg <AndreasSE93@gmail.com>
2015-06-02 19:11:44 +0200

    Added difficulty presets

--- app/src/main/java/devgui2/devgui2_project/DifficultySelect.java
+++ app/src/main/java/devgui2/devgui2_project/DifficultySelect.java
@@ -17,6 +17,38 @@ public class DifficultySelect extends Activity {
 		setContentView(R.layout.activity_difficulty_select);
 	}
 
+	public void play(View view) {
+		int gridWidth, gridHeight, blockMinSize, blockMaxSize;
+		switch (view.getId()) {
+			case R.id.easyButton:
+				gridWidth = 4;
+				gridHeight = 4;
+				blockMinSize = 2;
+				blockMaxSize = 3;
+				break;
+			case R.id.mediumButton:
+				gridWidth = 6;
+				gridHeight = 6;
+				blockMinSize = 3;
+				blockMaxSize = 4;
+				break;
+			case R.id.hardButton:
+				gridWidth = 8;
+				gridHeight = 8;
+				blockMinSize = 4;
+				blockMaxSize = 6;
+				break;
+			default:
+				throw new RuntimeException("Unknown play button pressed");
+		}
+		Intent intent = new Intent(this, GridGame.class);
+		intent.putExtra("gridWidth",    gridWidth);
+		intent.putExtra("gridHeight",   gridHeight);
+		intent.putExtra("blockMinSize", blockMinSize);
+		intent.putExtra("blockMaxSize", blockMaxSize);
+		startActivity(intent);
+	}
+
 	public void custom(View view) {
 		Intent intent = new Intent(this, CustomDifficulty.class);
 		startActivity(intent);
