98b2221d3cb1253d676ab591bc022ccfcaf93010
Andreas Wennerberg <AndreasSE93@gmail.com>
2015-06-09 20:31:52 +0200

    Removed unused code

--- app/src/main/java/devgui2/devgui2_project/MainScreen.java
+++ app/src/main/java/devgui2/devgui2_project/MainScreen.java
@@ -23,10 +23,4 @@ public class MainScreen extends Activity {
 		Intent intent = new Intent(this, DifficultySelect.class);
 		startActivity(intent);
 	}
-
-    @Override
-    public void onNewIntent(Intent intent) {
-        setIntent(intent);
-        onResume();
-    }
 }
